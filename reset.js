ef8d8e0c62f9aff80b045890bfe0e80b88fa61ac
Filip Chovanec <filip@chovanec.com>
2024-02-05 19:02:11 +0100

    Basic concept

---
+++ reset.js
@@ -0,0 +1,22 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+console.log("Loaded htmx-reset extension");
+
+function resetEventTrigger(event) {
+      const element = event.detail.requestConfig.elt;
+      element.reset();
+}
+
+htmx.defineExtension("reset-always", {
+  onEvent: function(name, event) {
+    if (name == "htmx:afterRequest") {
+      resetEventTrigger(event);
+    }
+  }
+});
+
+// TODO: implement reset-success reset-failure ...
